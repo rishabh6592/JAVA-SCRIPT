220e6fb2627ca4a34f8630f11b0bda3376861156
rishabh6592 <129681143+rishabh6592@users.noreply.github.com>
2024-09-10 00:29:30 +0530

    Var and Data type..

---
+++ datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+alert(3 + 3)  // we are using node js! not browser.
+
+
+/*
+DATA TYPE..
+
+number-  0 to .....
+string-  single quots / double quots.
+boolean- only true and false.
+null-  standalon value like EMPTY agr server se response nhi aya to that means NULL.
+undefine-  that means declear the variable but not put the value in variable, is called undefined.
+symbol-  that is use for uniqueness.
+
+object-  
+*/
---
+++ var.js
@@ -0,0 +1,19 @@
+// const accountId = 123456
+// let accountPassword = 121212
+// var accountEmail = "bank@rishabh.com"
+// accountCity = "delhi"
+
+// console.log(accountId , accountEmail , accountPassword , accountCity);
+
+accountCity = "mumbai"
+accountEmail = "mumbai@bank.com"
+accountId = 9999999 
+accountPassword = 5555
+let accountState;
+
+
+/*
+prefer not use to var
+because of issue in block scope and functional scope.
+*/
+console.table ( [accountCity , accountEmail , accountId , accountPassword ] )
