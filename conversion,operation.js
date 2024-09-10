41f0879592817993c9fa00d893b5f562145549b4
rishabh6592 <129681143+rishabh6592@users.noreply.github.com>
2024-09-10 14:56:20 +0530

    Conversion of Datatype

---
+++ conversion,operation.js
@@ -0,0 +1,45 @@
+// let score = null
+// let score = undefined
+// let score = "22abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => number
+// "33abc" => Nan
+// true => 1; false => 0
+
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rishabh" => true 
+
+
+
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
